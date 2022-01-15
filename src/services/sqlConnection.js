1aaa4389f3490f752ab008ab9316e5863fb93c4c
suvotheking007 <suvotheking007@gmail.com>
2022-01-15 21:32:08 +0530

    initial commit

--- src/services/sqlConnection.js
+++ src/services/sqlConnection.js
@@ -3,16 +3,16 @@ const Config = require("../constants/backendConfig");
 var pool = mysql.createPool(Config.mysql.prod);
 
 module.exports = {
-	executeQuery: function (sql, data, callback) {
-		pool.getConnection(function (err, connection) {
-			if (err) {
-				callback(err);
-			} else {
-				connection.query(sql, data, function (err1, results) {
-					connection.release();
-					callback(err1, results);
-				});
-			}
-		});
-	}
+  executeQuery: function (sql, data, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        callback(err);
+      } else {
+        connection.query(sql, data, function (err1, results) {
+          connection.release();
+          callback(err1, results);
+        });
+      }
+    });
+  },
 };
